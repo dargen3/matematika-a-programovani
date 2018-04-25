fce2295a0cddb3850be90ee41e1f1b01da164f5c
dargen3 <dargen3@centrum.cz>
2018-04-25 19:58:42 +0200

    from terminal

--- uprava.py
+++ uprava.py
@@ -2,4 +2,4 @@ import sys
 import fileinput
 with fileinput.FileInput(sys.argv[1], inplace=True) as file:
     for line in file:
-        print(line.replace("<svg>", "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><svg  width=\"100%\" height=\"100%\" viewBox=\"0 0 1000 1000\"  xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"), end='')
+        print(line.replace("<svg>", "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><svg  width=\"100%\" height=\"100%\" viewBox=\"0 0 100 100\"  width=\"100%\" height=\"100%\" viewBox=\"0 0 1000 1000\"  xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"), end='')
